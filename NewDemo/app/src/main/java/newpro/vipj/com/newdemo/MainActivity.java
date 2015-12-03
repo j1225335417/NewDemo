11633ffa8fa11e154744bbf335af75f1b72b28de
jiang.gaungjin@niuhome.com <vip1225335417>
2015-12-03 10:46:23 +0800

    111

--- NewDemo/app/src/main/java/newpro/vipj/com/newdemo/MainActivity.java
+++ NewDemo/app/src/main/java/newpro/vipj/com/newdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package newpro.vipj.com.newdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
